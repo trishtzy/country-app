6f82a73bdc2a74a5437b5418801b3bd3289b623d
Tricia Tan Zi Ying <tricia.tzy@tutanota.com>
2021-03-13 14:46:49 +0800

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const outputDir = path.join(__dirname, "build/")
+
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = {
+  entry: "./src/Index.bs.js",
+  mode: isProd ? "production" : "development",
+  devtool: "source-map",
+  output: {
+    path: outputDir,
+    filename: "Index.js",
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: false
+    })
+  ],
+  devServer: {
+    compress: true,
+    contentBase: outputDir,
+    port: process.env.PORT || 8000,
+    historyApiFallback: true
+  }
+}
